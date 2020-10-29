d4c12d1c1c38abd66fc423350d6524e302d7a5c8
Jerry Balderas <Gerardo.Balderas@cognizant.com>
2020-10-29 00:13:57 -0500

    Add Index Page

--- src/main/java/codecamp/bug/wars/api/gateway/BugWarsUiApplication.java
+++ src/main/java/codecamp/bug/wars/api/gateway/BugWarsUiApplication.java
@@ -1,17 +1,32 @@
 package codecamp.bug.wars.api.gateway;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableZuulProxy
 @EnableEurekaClient
+@RestController
 public class BugWarsUiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BugWarsUiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BugWarsUiApplication.class, args);
+    }
+
+    @Value("${eureka.instance.hostname:}")
+    String hostname;
+
+    @Value("${eureka.client.serviceUrl.defaultZone:")
+    String eurekaUrl;
+
+    @GetMapping
+    public String getIndex() {
+        return "Bug Wars API Gateway: " + hostname + ":" + eurekaUrl;
+    }
 
 }
